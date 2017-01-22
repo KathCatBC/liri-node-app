530674377297213c554f3d4325182f36b8b928f9
Kathleen Catlett <kathleencatlett@comcast.net>
2017-01-22 10:23:02 -0500

    pseudo code

---
+++ liri.js
@@ -0,0 +1,52 @@
+
+
+var doThis = process.argv[2];
+var withThis = process.argv[3]
+
+if (doThis = do-what-it-says) {
+	//get data from random.txt
+	// change doThis to what is in random.txt
+	//make withThis to what is in random.txt
+}
+
+
+switch(doThis) {
+    case "my-tweets":
+    	// get 20 latest tweets
+        break;
+    case "spotify-this-song":
+    	if (withThis = "") {
+    		withThis =  // "The Sign" by Ace of Base
+    	}
+		// Artist(s)
+		// The song's name
+		// A preview link of the song from Spotify
+		// The album that the song is from
+		// if no song is provided then your program will default to
+        break;
+    case "movie-this":
+    	if (withThis = "") {
+    		withThis =  'Mr. Nobody.'
+    	}}
+  		//Title of the movie.
+		// Year the movie came out.
+		// IMDB Rating of the movie.
+		// Country where the movie was produced.
+		// Language of the movie.
+		// Plot of the movie.
+		// Actors in the movie.
+		// Rotten Tomatoes Rating.
+		// Rotten Tomatoes URL.
+
+    	break;
+    
+    default 
+    	console.log("Sorry, that command is not recognized")
+}
+
+
+// In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
+
+// Make sure you append each command you run to the log.txt file.
+
+// Do not overwrite your file each time you run a command.
