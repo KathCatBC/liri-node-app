6403078dcd118f27c1c3595fd77be86598b28788
Kathleen Catlett <kathleencatlett@comcast.net>
2017-01-30 19:59:52 -0500

    added logging

--- liri.js
+++ liri.js
@@ -15,6 +15,7 @@ if (doThis == "do-what-it-says") {
 	    doOutput();
      });
 } else {
+	
 	doOutput();
 }
 
@@ -35,20 +36,29 @@ function doOutput() {
   				access_token_secret: apiKeys.twitterKeys.access_token_secret
 			});
 
+			fs.appendFile('log.txt', '{' + process.argv[2] + ":" + process.argv[3] + "}, generated the following results:\r\n");
+
 			var params = {screen_name: 'KathCat07', count: 20};
 			client.get('statuses/user_timeline', params, function(error, tweets, response) {
   				
   				if (!error) {
-  					console.log("# of tweets = " + tweets.length);
-  					console.log("**************************************");
+  					var outputData = "";
+  					outputData += "# of tweets = " + tweets.length + "\r\n"
+  					outputData += "**************************************\r\n";
    					for (i=0; i<=tweets.length-1; i++) {
-   						console.log("on " + tweets[i].created_at);
-   						console.log("I tweeted:  " + tweets[i].text);
-   						console.log("**************************************");
+   						outputData += "on " + tweets[i].created_at +"\r\n";
+   						outputData += "I tweeted:  " + tweets[i].text +"\r\n";
+   						outputData +=  "**************************************\r\n"
    					} 
+   					outputData += "+++++++++++++++++++++++++++++++++++++++++\r\n";
+   					console.log(outputData);
+   					fs.appendFile("log.txt", outputData)
    				} else {
-   					 console.log("oops... there was an error"); 
-  				} 
+   					 console.log("oops... there was an error");
+   					 fs.appendFile("oops... there was an error\r\n"); 
+   					 fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
+  				}
+  				
 			});
 	    	
 	        break;
@@ -60,17 +70,29 @@ function doOutput() {
 	    	}
 
 			var spotify = require('spotify');
+
+			fs.appendFile('log.txt', '{' + process.argv[2] + ":" + process.argv[3] + "}, generated the following results: \r\n");
  
   			spotify.search({ type: 'track', query: withThis }, function(err, data) {
     			if ( err ) {
         			console.log('Error occurred: ' + err);
+        			fs.appendFile('log.txt', 'Error occurred: ' + err + "\r\n");
+        			fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
     			} else {
-					console.log("song:  " +    data.tracks.items[0].name);  
-     				console.log("preview:  " + data.tracks.items[0].preview_url);
-     				console.log("album:  " + 	 data.tracks.items[0].album.name);
-     				console.log("artists:  " + data.tracks.items[0].artists[0].name);
+	    			var outputData = {
+						song:  data.tracks.items[0].name,
+		 				preview: data.tracks.items[0].preview_url,
+		 				album: data.tracks.items[0].album.name,
+		 				artists: data.tracks.items[0].artists[0].name
+     				}
+
+     				console.log(JSON.stringify(outputData, null, 2));
+     				fs.appendFile('log.txt', outputData + "\r\n");
+     				fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
     			};
   			});
+
+  			
 
 	        break;
 
@@ -80,24 +102,34 @@ function doOutput() {
 	    		withThis =  'Mr Nobody'
 	    	}
 
+	    	fs.appendFile('log.txt', '{' + process.argv[2] + ":" + process.argv[3] + "}, generated the following results:\r\n");
+			
 			var queryUrl = "http://www.omdbapi.com/?t=" + withThis + "&y=&plot=short&tomatoes=true&r=json";
-
 			req(queryUrl, function (er, res, body) {
 
 				if (er !== null) {
 					console.log("oops... there was an error"); 
+					fs.appendFile('log.txt', "oops... there was an error\r\n"); 
+					fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
 				} else if (JSON.parse(body).Response == "False"){
 					console.log("Error - " + withThis + " was not found!")
+					fs.appendFile('log.txt', "Error - " + withThis + " was not found!\r\n")
+					fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
 				} else {
-				    console.log("Title:  " + JSON.parse(body).Title);
-				    console.log("Year:  " + JSON.parse(body).Year);
-				    console.log("IMDB Rating:  " + JSON.parse(body).imdbRating);
-				    console.log("Country:  " + JSON.parse(body).Country);
-				    console.log("Language:  " + JSON.parse(body).Language);
-				    console.log("Plot:  " + JSON.parse(body).Plot);
-				    console.log("Actors:  " + JSON.parse(body).Actors);
-				    console.log("Rotten Tomatoes Rating:  " + JSON.parse(body).tomatoMeter);
-				    console.log("Rotten Tomatoes URL:  " + JSON.parse(body).tomatoURL);
+					var outputData = {
+					    Title: JSON.parse(body).Title,
+					    Year:  JSON.parse(body).Year,
+					    "IMDB Rating": JSON.parse(body).imdbRating,
+					    Country: JSON.parse(body).Country,
+					    Language: JSON.parse(body).Language,
+					    Plot: JSON.parse(body).Plot,
+					    Actors: JSON.parse(body).Actors,
+					    "Rotten Tomatoes Rating": JSON.parse(body).tomatoMeter,
+					    "Rotten Tomatoes URL":  JSON.parse(body).tomatoURL
+					}
+					console.log(JSON.stringify(outputData, null, 2));
+					fs.appendFile('log.txt', outputData + "\r\n");
+					fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
 				}
 
 			});
@@ -106,7 +138,7 @@ function doOutput() {
 
 	    default:
 	    	console.log("Sorry, that command is not recognized");
+	    	fs.appendFile('log.txt', "Sorry, that command is not recognized\r\n");
+	    	fs.appendFile('log.txt', "+++++++++++++++++++++++++++++++++++++++++\r\n");
 	}
-
-	fs.appendFile('log.txt', '{' + process.argv[2] + ":" + process.argv[3] + "}, ");
 }
